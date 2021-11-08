1d438843cb98929b73fcc86ce19f60150904f1a8
aleksan22 <80626790+aleksan22@users.noreply.github.com>
2021-11-08 11:56:10 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let x = prompt('Какой Вам нужен сайт? Варианты: 0 - визитка, 1 - лендинг');
+/*let x = prompt('Какой Вам нужен сайт? Варианты: 0 - визитка, 1 - лендинг');
 let y = prompt('Нужна ли Вам адаптивная версия Вашего сайта? Варианты: 0 - да, 1 - нет');
 
 let sale = ["Стоимость заказа: 1000р", "Стоимость заказа: 1500р", "Стоимость заказа: 2000р"];
@@ -18,10 +18,25 @@ if(x == 0 && y == 0){
     else{
         alert('Обновите страницу и выберите необходимые варианты ответов!')
     }
-
+*/
   $('a[href^="#"]').click(function(){
     let elClick = $(this).attr("href");
     $('body').animate({scrollTop: $(elClick).position().top + $('body').scrollTop()}, 1500)
     
     return false;
   });  
+  
+  $(document).ready(function(){
+    $('.slick__slider').slick({
+        arrows:true, // показать стрелки
+        dots: true
+    })
+  });
+
+  $(document).ready(function(){
+    $('.slick').slick({
+        dots: true,
+        prevArrow:'<img src="img/left.png" class="slick-prev" width="25px" height="25px">',
+        nextArrow:'<img src="img/right.png" class="slick-next" width="25px" height="25px">'
+    })
+  });
